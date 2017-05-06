eb619c2de541598f52cccadad5d0b535bb13837d
ramen <mesoramen3@gmail.com>
2017-05-05 18:20:55 -0700

    Added phone.tax.js to the repo

---
+++ phone_tax.js
@@ -0,0 +1,31 @@
+// this program will calculate the total price of a user's phone purchase.
+//
+// make sure that the user is allowed to enter:
+// --total spending budget
+// and the user is allowed to choose from a list of radio buttons:
+// --phone type
+// --phone accessories
+//
+// after the user has selected a phone type and phone accessories, 
+// apply the relevant state and local taxes, and present the total cost
+// to the user.
+//
+// if the total amount of a phone purchase excedes the user's total 
+// budget, postpone the transaction and alert the user.
+
+// establish the sales tax (global var), and store the total spending 
+// limit for the user.
+function submit() {
+/*
+const SALES_TAX = 0.835;
+const TOTAL_SPENDING_LIMIT = 0.0;
+
+// store the total spending budget in relevent variable
+var budget = document.getElementById("budget");
+TOTAL_SPENDING_LIMIT = budget;
+// output to console to verify functionality
+console.log(TOTAL_SPENDING_LIMIT); 
+*/
+console.log("log");
+}
+// end of document
